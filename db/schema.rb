4dc0bf5e35bca5b863f96c556ed888efc9b76e19
Ray Villaraza <ray.villaraza@strongmind.com>
2024-06-28 16:19:02 -0700

    users

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_28_010740) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_28_231010) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "firstName"
+    t.string "lastName"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
